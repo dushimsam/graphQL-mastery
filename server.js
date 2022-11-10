401d324b3fe7449cf093579d7d02038b46d7f7a6
dushimsam <dushsam@gmail.com>
2022-11-10 12:36:32 +0200

    feat(starter): high-level graphQl version 1, get the list of books

--- server.js
+++ server.js
@@ -1,22 +1,50 @@
 const express = require('express')
 const {graphqlHTTP} = require('express-graphql')
-const { GraphQLSchema, GraphQLString, GraphQLObjectType } = require('graphql')
+const { GraphQLSchema, GraphQLString, GraphQLNonNull, GraphQLObjectType, GraphQLList, GraphQLInt } = require('graphql')
 const app =  express()
 
-const schema = new GraphQLSchema({
-  query: new GraphQLObjectType({
-    name:"HelloWorld",
-    fields: () =>({
-        message:{
-           type: GraphQLString,
-           resolve: () => "Hello World" 
-        },
-        resp:{
-            type: GraphQLString,
-            resolve: () =>"Hey man"
+const authors = [
+	{ id: 1, name: 'J. K. Rowling' },
+	{ id: 2, name: 'J. R. R. Tolkien' },
+	{ id: 3, name: 'Brent Weeks' }
+]
+
+const books = [
+	{ id: 1, name: 'Harry Potter and the Chamber of Secrets', authorId: 1 },
+	{ id: 2, name: 'Harry Potter and the Prisoner of Azkaban', authorId: 1 },
+	{ id: 3, name: 'Harry Potter and the Goblet of Fire', authorId: 1 },
+	{ id: 4, name: 'The Fellowship of the Ring', authorId: 2 },
+	{ id: 5, name: 'The Two Towers', authorId: 2 },
+	{ id: 6, name: 'The Return of the King', authorId: 2 },
+	{ id: 7, name: 'The Way of Shadows', authorId: 3 },
+	{ id: 8, name: 'Beyond the Shadows', authorId: 3 }
+]
+
+
+const BookType = new GraphQLObjectType({
+    name: 'Book',
+    description: 'This represents a book written by the author',
+    fields:()=>({
+        id: { type:new GraphQLNonNull(GraphQLInt)},
+        name: { type:new GraphQLNonNull(GraphQLString)},
+        authorId: {type:new GraphQLNonNull(GraphQLInt)}
+    })
+})
+
+const RootQuery = new GraphQLObjectType({
+    name:"Query",
+    description:"Root query",
+    fields:()=>({
+        books: {
+            type: new GraphQLList(BookType),
+            description:" List of All books",
+            resolve: () => books,
         }
     })
 })
+
+const schema = new GraphQLSchema({
+  query: RootQuery
 })
 
 app.use('/graphql',graphqlHTTP({
